713e26eba99761f33a496064da557331ca1718b2
haha4894 <904679083@qq.com>
2024-12-18 18:14:12 +0800

    Add files via upload

---
+++ src/views/Layout/index.vue
@@ -0,0 +1,30 @@
+<script setup>
+import LayoutHeader from './components/LayoutHeader.vue';
+import LayoutMeum from './components/LayoutMeum.vue';
+import LayoutGraph from './components/LayoutGraph.vue';
+
+</script>
+
+<template>
+  <div class="common-layout">
+    <el-container>
+      <el-header height="150px" class="header">
+        <LayoutHeader></LayoutHeader>
+      </el-header>
+      <el-container>
+        <el-aside width="200px">
+          <LayoutMeum/>
+        </el-aside>
+        <el-main><LayoutGraph/></el-main>
+        <!-- <el-main>Main</el-main> -->
+      </el-container>
+    </el-container>
+  </div>
+
+</template>
+
+<style scoped lang='scss'>
+  .header{
+    padding: 0px;
+  }
+</style>
